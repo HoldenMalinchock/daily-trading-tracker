91a40676ec7422c8469bd410c6e8ca22443e42b9
Holden Malinchock <h.malinchock@verizon.net>
2024-09-27 22:18:49 -0400

    Removing the account call

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="flex justify-center">
-        Welcome to the home page where I will track my trading bot progress publicly.
-        <div>
-            {{ account }}
-        </div>
-    </div>
+  <div class="flex justify-center">
+    Welcome to the home page where I will track my trading bot progress publicly.
+  </div>
 </template>
 
 <script setup lang="ts">
-const { data: account } = useFetch("/api/getAccount")
 </script>
