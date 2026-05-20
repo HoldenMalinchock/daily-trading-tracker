9c09e03d2527a03e2c2e5de950a9c291a04b01cf
Holden Malinchock <h.malinchock@verizon.net>
2026-05-19 23:37:01 -0400

    REmoving pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@pinia/nuxt"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
   eslint: {
     config: {
       stylistic: {
